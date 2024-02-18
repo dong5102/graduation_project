b5b68c7ed46eac3f1835995190a53617561a4656
dong5102 <80802452+dong5102@users.noreply.github.com>
2024-02-19 00:19:05 +0900

    Codes

---
+++ Player.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed;
+    public Scanner scanner;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        scanner = GetComponent<Scanner>();
+    }
+ 
+    void Update()
+    {
+        if (!GameManager.instance.isLive)
+            return;
+
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        if (!GameManager.instance.isLive)
+            return;
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime; // fixedDeltaTime <<물리 프레임 하나가 소비한 시간
+        // 3. 위치 이동
+        rigid.MovePosition(rigid.position + nextVec); // (현재위치 + 벡터)
+    }
+
+    void LateUpdate() // 프레임이 끝나기 직전에 실행되는 생명주기 함수
+    {
+        if (!GameManager.instance.isLive)
+            return;
+
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0) {
+            spriter.flipX = inputVec.x < 0; // x축 이동이 마이너스일때 플립x가 true로 바뀐다.
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (!GameManager.instance.isLive)
+            return;
+
+        GameManager.instance.health -= Time.deltaTime * 10;
+
+        if (GameManager.instance.health < 0)
+        {
+            for (int index=2; index < transform.childCount; index++)
+            {
+                transform.GetChild(index).gameObject.SetActive(false);
+            }
+
+            GameManager.instance.GameOver();
+        }
+    }
+}
